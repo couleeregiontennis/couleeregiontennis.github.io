f9cd6b6f3c0efb61761cedd8f5b60c49acccef09
brett <brett.meddaugh@gmail.com>
2025-12-09 08:45:33 -0600

    reverting from babel

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,19 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import babel from '@rollup/plugin-babel';
 
 export default defineConfig({
-  plugins: [
-    react(),
-    babel({
-      presets: ['@babel/preset-env', '@babel/preset-react'],
-      extensions: ['.jsx', '.js'],
-    }),
-  ],
+  plugins: [react()],
+  esbuild: {
+    loader: 'jsx',
+    include: /src\/.*\.jsx?$/,
+    exclude: [],
+  },
+  optimizeDeps: {
+    esbuildOptions: {
+      loader: {
+        '.js': 'jsx',
+        '.jsx': 'jsx',
+      },
+    },
+  },
 });
