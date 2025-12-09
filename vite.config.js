0dc4ab1640bb598c091f6ecce0c0d721356c901f
brett <brett.meddaugh@gmail.com>
2025-12-09 06:41:23 -0600

    updated to use babel and gitignore

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,13 @@
-// vite.config.js
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import babel from '@rollup/plugin-babel';
 
 export default defineConfig({
-  plugins: [react()],
-  base: './',
-  build: {
-    outDir: 'build'
-  }
+  plugins: [
+    react(),
+    babel({
+      presets: ['@babel/preset-env', '@babel/preset-react'],
+      extensions: ['.jsx', '.js'],
+    }),
+  ],
 });
